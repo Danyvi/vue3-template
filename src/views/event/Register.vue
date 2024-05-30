b10f28c5c9a8308e787b87ecf72ab2329df3556f
Danyvi <vinci.dani@gmail.com>
2024-05-30 14:34:59 -0400

    programmatic router

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
 // const props = defineProps(['event'])
 const props = defineProps({
   event: {
@@ -7,11 +11,21 @@ const props = defineProps({
   }
 })
 
+const register = () => {
+  console.log('Registering for the event')
+  // if registration API call is successful
+  // redirect to event-details
+  router.push({
+    name: 'event-details'
+  })
+}
+
 </script>
 
 <template>
   <div class="event-details">
-    Register for the event here
+    <p>Register for the event here</p>
+    <button @click="register">Register me</button>
   </div>
 </template>
 
